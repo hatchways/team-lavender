29fb2f03323847a18678efd85e6aeea01b34984b
atsushiflee <atsushiflee@icloud.com>
2020-08-11 11:09:44 -0700

    Created Appointments Model

---
+++ server/models/Appointments.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const AppointmentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  time: {
+    type: Date,
+    require: true,
+  },
+  timezone: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("Appointments", AppointmentSchema);
