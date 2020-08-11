223e13ba0bdf44d3416cddab682f6f45e9024e3f
Aditya Sharma <sharmadityaa@gmail.com>
2020-08-11 15:34:23 -0400

    .env file has database URI

--- server/middleware/database.js
+++ server/middleware/database.js
@@ -1,7 +1,8 @@
 const MongoClient = require("mongodb").MongoClient;
-require("dotenv").config;
+
 const database = "calendly";
 const collections = "test";
+
 const uri =
   "mongodb+srv://team-lavender:lavender-team@cluster0.kenuz.mongodb.net/calendly?retryWrites=true&w=majority";
 // put the URI in process.env.ATLAS_URI, but not working for some reason
