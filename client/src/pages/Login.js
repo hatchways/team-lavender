4359ced0eef89f238e14400413441ede7421ae69
Younhee Lee <younheelee@Younhees-MacBook-Pro.local>
2020-08-11 15:30:57 -0700

    replace class components to functional component in login and singup pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 import { Button, Divider, TextField } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import { Route, Link } from "react-router-dom";
 
-const LoginPageStyle = () => ({
+const LoginPageStyle = (theme) => ({
 //step 1
+  title : {
+    display : "flex",
+    "font-size" : "20px",
+    "font-weight" : "bold",
+    "justify-content" : "center",
+    "margin" : "50px"
+  },
   signupContainer: {
     margin  :   "auto",
     width : "400px",
     height : "480px",
     "-webkit-box-shadow" : "1px 1px 10px 3px rgba(196,196,196,1)",
     font : "Arial, Helvetica, sans-serif",
-    "font-weight" : "bold",
+    "font-weight" : "bold",     
     "text-align" : "center"
   },
   aboveDivider : {
@@ -56,13 +63,6 @@ const LoginPageStyle = () => ({
     color : "#F78104", 
     "text-decoration": "none"
   },
-  title : {
-    display : "flex",
-    "font-size" : "20px",
-    "font-weight" : "bold",
-    "justify-content" : "center",
-    "margin" : "50px"
-  },
 
 // step 2
   gmailSignup : {
@@ -85,56 +85,44 @@ const LoginPageStyle = () => ({
   }
 });
 
-class LoginPage extends Component {
-    state = {
-        step : 1
-    };
+function LoginPage (props) {
+    const [step, setStep] = useState(1);
+    const { classes } = props;
 
-    switchStep = () => {
-        if (this.state.step == 1) {
-            this.setState(prevState => ({ step: (prevState.step = 2) }));
-        } else {
-            this.setState(prevState => ({ step: (prevState.step = 1) }));
-        }
-    };
-
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <div>
-                <div className={classes.title}><div style={{color : "#F78104"}}>calend</div>App</div>
-                {this.state.step == 1 && (
-                    <div className={classes.signupContainer}>
-                        <div className={classes.aboveDivider}>
-                            <div className={classes.firstDivAboveDivider}>Sign up with CalendApp</div>
-                            <div className={classes.secondDivAboveDivider}>
-                                <div className={classes.firstDivOfSecondDivAboveDivider}>Enter your e-mail to get started</div>
-                                <TextField className={classes.textFieldOfSecondDivAboveDivider} label="E-mail address" variant="outlined"></TextField>
-                            </div>
-                            <Button className={classes.startButton} onClick={this.switchStep}>Continue</Button>
+    return (
+        <div>
+            <div className={classes.title}><div style={{color : "#F78104"}}>calend</div>App</div>
+            {step == 1 && (
+                <div className={classes.signupContainer}>
+                    <div className={classes.aboveDivider}>
+                        <div className={classes.firstDivAboveDivider}>Sign up with CalendApp</div>
+                        <div className={classes.secondDivAboveDivider}>
+                            <div className={classes.firstDivOfSecondDivAboveDivider}>Enter your e-mail to get started</div>
+                            <TextField className={classes.textFieldOfSecondDivAboveDivider} label="E-mail address" variant="outlined"></TextField>
                         </div>
-                        <Divider />
-                        <div className={classes.belowDivider}>Don't have account?
-                            <Link className={classes.signupButton} to={{pathname: "/"}}>Sign up</Link>
-                        </div>
+                        <Button className={classes.startButton} onClick={() => setStep(2)}>Continue</Button>
                     </div>
-                )}
-                {this.state.step == 2 && (
-                    <div className={classes.signupContainer}>
-                        <div className={classes.aboveDivider}>
-                            <div className={classes.firstDivAboveDivider}>Welcome back<br></br>john-doe@gmail.com</div>
-                            <Button className={classes.gmailSignup}>Login with Google</Button>
-                        </div>
-                        <Divider />
-                        <div className={classes.belowDividerStepTwo}>Don't have an account?
-                            <Link className={classes.buttonbelowDividerStepTwo} to={{pathname: "/"}}>Sign up</Link>
-                        </div>
+                    <Divider />
+                    <div className={classes.belowDivider}>Don't have account?
+                        <Link className={classes.signupButton} to={{pathname: "/"}}>Sign up</Link>
                     </div>
-                )}
-            </div>
-        );
-    }
+                </div>
+            )}
+            {step == 2 && (
+                <div className={classes.signupContainer}>
+                    <div className={classes.aboveDivider}>
+                        <div className={classes.firstDivAboveDivider}>Welcome back<br></br>john-doe@gmail.com</div>
+                        <Button className={classes.gmailSignup}>Login with Google</Button>
+                    </div>
+                    <Divider />
+                    <div className={classes.belowDividerStepTwo}>Don't have an account?
+                        <Link className={classes.buttonbelowDividerStepTwo} to={{pathname: "/"}}>Sign up</Link>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+    
 }
 
 export default withStyles(LoginPageStyle)(LoginPage);
--- client/src/pages/Singup.js
+++ client/src/pages/Singup.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component,useState } from "react";
 
 import { Button, Divider, TextField } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
@@ -93,60 +93,46 @@ const SignupPageStyle = () => ({
   }
 });
 
-class SignupPage extends Component {
-    state = {
-        step : 2
-    };
-
-    switchStep = () => {
-        if (this.state.step == 1) {
-            this.setState(prevState => ({ step: (prevState.step = 2) }));
-        } else {
-            this.setState(prevState => ({ step: (prevState.step = 1) }));
-        }
-    };
-
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <div>
-                <div className={classes.title}><div style={{color : "#F78104"}}>calend</div>App</div>
-                {this.state.step == 1 && (
-                    <div className={classes.signupContainer}>
-                        <div className={classes.aboveDivider}>
-                            <div className={classes.firstDivAboveDivider}>Sign up with CalendApp</div>
-                            <div className={classes.secondDivAboveDivider}>
-                                <div className={classes.firstDivOfSecondDivAboveDivider}>Enter your e-mail to get started</div>
-                                <TextField className={classes.textFieldOfSecondDivAboveDivider} label="E-mail address" variant="outlined"></TextField>
-                            </div>
-                            <Button className={classes.startButton} onClick={this.switchStep}>Get started</Button>
+function SignupPage (props) {
+    const [step, setStep] = useState(1);
+    const { classes } = props;
+    return (
+        <div>
+            <div className={classes.title}><div style={{color : "#F78104"}}>calend</div>App</div>
+            {step == 1 && (
+                <div className={classes.signupContainer}>
+                    <div className={classes.aboveDivider}>
+                        <div className={classes.firstDivAboveDivider}>Sign up with CalendApp</div>
+                        <div className={classes.secondDivAboveDivider}>
+                            <div className={classes.firstDivOfSecondDivAboveDivider}>Enter your e-mail to get started</div>
+                            <TextField className={classes.textFieldOfSecondDivAboveDivider} label="E-mail address" variant="outlined"></TextField>
                         </div>
-                        <Divider />
-                        <div className={classes.belowDivider}>Already have an account? 
-                            <Link className={classes.loginButton} to={{pathname: "/login"}}>Log in</Link>
-                        </div>
+                        <Button className={classes.startButton} onClick={() => setStep(2)}>Get started</Button>
                     </div>
-                )}
-                {this.state.step == 2 && (
-                    <div className={classes.signupContainer}>
-                        <div className={classes.aboveDivider}>
-                            <div className={classes.firstDivAboveDivider}>Hi john-doe@gmail.com</div>
-                            <div className={classes.signupDescription}>
-                                The easist way for you to sign up is with Google.
-                                This will automatically connect your calendar so you can start using CalendarApp right away!
-                            </div>
-                            <Button className={classes.gmailSignup}>Sign up with Google</Button>
-                        </div>
-                        <Divider />
-                        <div className={classes.belowDividerStepTwo}>Prefer to create an account with a password?
-                            <Button className={classes.buttonbelowDividerStepTwo} onClick={this.switchStep} >Click here</Button>
-                        </div>
+                    <Divider />
+                    <div className={classes.belowDivider}>Already have an account? 
+                        <Link className={classes.loginButton} to={{pathname: "/login"}}>Log in</Link>
                     </div>
-                )}
-            </div>
-        );
-    }
+                </div>
+            )}
+            {step == 2 && (
+                <div className={classes.signupContainer}>
+                    <div className={classes.aboveDivider}>
+                        <div className={classes.firstDivAboveDivider}>Hi john-doe@gmail.com</div>
+                        <div className={classes.signupDescription}>
+                            The easist way for you to sign up is with Google.
+                            This will automatically connect your calendar so you can start using CalendarApp right away!
+                        </div>
+                        <Button className={classes.gmailSignup}>Sign up with Google</Button>
+                    </div>
+                    <Divider />
+                    <div className={classes.belowDividerStepTwo}>Prefer to create an account with a password?
+                        <Button className={classes.buttonbelowDividerStepTwo} onClick={() => setStep(1)} >Click here</Button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default withStyles(SignupPageStyle)(SignupPage);
