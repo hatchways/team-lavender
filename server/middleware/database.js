6f5be84cbd18390e367899a445f6ee7ee4bd0e77
Aditya Sharma <sharmadityaa@gmail.com>
2020-08-11 11:55:47 -0400

    Database setup

---
+++ server/middleware/database.js
@@ -0,0 +1,18 @@
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config;
+const database = "calendly";
+const collections = "test";
+const uri =
+  "mongodb+srv://team-lavender:lavender-team@cluster0.kenuz.mongodb.net/calendly?retryWrites=true&w=majority";
+// put the URI in process.env.ATLAS_URI, but not working for some reason
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to Atlas MongoDB");
+  const collection = client.db(database).collection(collections);
+  // perform actions on the collection object
+  client.close();
+});
