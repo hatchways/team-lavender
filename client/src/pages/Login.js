c04f17e74c556dc08934feff175611ddc2c85128
Mila Mamat <mila.windsor@gmail.com>
2020-08-12 15:01:04 -0400

    integrate loginbtn into login/signup page
    . added access token and expire at to localStorage
    . login btn is added to the pages
    . login page wil auto check for useremail from local storage

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,48 +1,93 @@
-import React, { Component, useState } from "react";
+import React, {useState, useEffect } from "react";
 
 import { Button, Divider, TextField } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
-import { Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LoginPageStyle from "../css/LoginCss";
+import LoginBtn from "../components/LoginBtn";
 
-function LoginPage (props) {
-    const [step, setStep] = useState(1);
-    const { classes } = props;
+function LoginPage(props) {
+  const [step, setStep] = useState({
+    step: 1,
+    email: "",
+  });
+  const { classes } = props;
+  let userEmail;
 
-    return (
-        <div>
-            <div className={classes.title}><div style={{color : "#F78104"}}>calend</div>App</div>
-            {step == 1 && (
-                <div className={classes.signupContainer}>
-                    <div className={classes.aboveDivider}>
-                        <div className={classes.firstDivAboveDivider}>Sign up with CalendApp</div>
-                        <div className={classes.secondDivAboveDivider}>
-                            <div className={classes.firstDivOfSecondDivAboveDivider}>Enter your e-mail to get started</div>
-                            <TextField className={classes.textFieldOfSecondDivAboveDivider} label="E-mail address" variant="outlined"></TextField>
-                        </div>
-                        <Button className={classes.startButton} onClick={() => setStep(2)}>Continue</Button>
-                    </div>
-                    <Divider />
-                    <div className={classes.belowDivider}>Don't have account?
-                        <Link className={classes.signupButton} to={{pathname: "/"}}>Sign up</Link>
-                    </div>
-                </div>
-            )}
-            {step == 2 && (
-                <div className={classes.signupContainer}>
-                    <div className={classes.aboveDivider}>
-                        <div className={classes.firstDivAboveDivider}>Welcome back<br></br>john-doe@gmail.com</div>
-                        <Button className={classes.gmailSignup}>Login with Google</Button>
-                    </div>
-                    <Divider />
-                    <div className={classes.belowDividerStepTwo}>Don't have an account?
-                        <Link className={classes.buttonbelowDividerStepTwo} to={{pathname: "/"}}>Sign up</Link>
-                    </div>
-                </div>
-            )}
+  useEffect(() => {
+      //check localStorage for user email, if exists, move to step 2
+    userEmail = localStorage.getItem("googleEmail");
+    if (userEmail)
+      setStep({
+        step: 2,
+        email: userEmail,
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.title}>
+        <div style={{ color: "#F78104" }}>calend</div>App
+      </div>
+      {step.step === 1 && (
+        <div className={classes.signupContainer}>
+          <div className={classes.aboveDivider}>
+            <div className={classes.firstDivAboveDivider}>
+              Sign up with CalendApp
+            </div>
+            <div className={classes.secondDivAboveDivider}>
+              <div className={classes.firstDivOfSecondDivAboveDivider}>
+                Enter your e-mail to get started
+              </div>
+              <TextField
+                className={classes.textFieldOfSecondDivAboveDivider}
+                label="E-mail address"
+                variant="outlined"
+                onChange={(e) => setStep({ ...step, email: e.target.value })}
+              ></TextField>
+            </div>
+            <Button
+              className={classes.startButton}
+              onClick={() => setStep({ ...step, step: 2 })}
+            >
+              Continue
+            </Button>
+          </div>
+          <Divider />
+          <div className={classes.belowDivider}>
+            Don't have account?
+            <Link className={classes.signupButton} to={{ pathname: "/" }}>
+              Sign up
+            </Link>
+          </div>
         </div>
-    );
-    
+      )}
+      {step.step === 2 && (
+        <div className={classes.signupContainer}>
+          <div className={classes.aboveDivider}>
+            <div className={classes.firstDivAboveDivider}>
+              Welcome back<br></br>
+              {step.email}
+            </div>
+            <LoginBtn
+              className={classes.gmailSignup}
+              loginText="Login with Google"
+            />
+          </div>
+          <Divider />
+          <div className={classes.belowDividerStepTwo}>
+            Don't have an account?
+            <Link
+              className={classes.buttonbelowDividerStepTwo}
+              to={{ pathname: "/" }}
+            >
+              Sign up
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default withStyles(LoginPageStyle)(LoginPage);
