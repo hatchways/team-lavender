74d98fad1cece8e3675b112cfe93cafec04b85f8
Aditya Sharma <sharmadityaa@gmail.com>
2020-08-12 13:05:06 -0400

    Moved database to server folder and .env URI works

--- server/database.js
+++ server/database.js
@@ -1,11 +1,12 @@
 const MongoClient = require("mongodb").MongoClient;
 
+require("dotenv").config();
+
 const database = "calendly";
 const collections = "test";
 
-const uri =
-  "mongodb+srv://team-lavender:lavender-team@cluster0.kenuz.mongodb.net/calendly?retryWrites=true&w=majority";
-// put the URI in process.env.ATLAS_URI, but not working for some reason
+const uri = process.env.ATLAS_URI;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
