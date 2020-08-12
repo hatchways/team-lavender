1f8dca50febdd4baa11e1644afd0087f3d4b7f5f
Mila Mamat <mila.windsor@gmail.com>
2020-08-11 21:06:43 -0400

    remove clientID from env

--- client/src/components/GoogleBtn.js
+++ client/src/components/GoogleBtn.js
@@ -6,25 +6,6 @@ import Logout from "./Logout"
 function GoogleBtn(props) {
   let clientID = "871373961261-rjej65g97dc3o6jiuflq6s2gp5v9ptut.apps.googleusercontent.com"
 
-//   let [clientID,setClientID] =useState("")
-
-// useEffect(()=>{
-//   fetch("/auth")
-//   .then(res => {
-//     console.log(res);
-//     if (res.status === 200) return res.json();
-//     else throw Error("OAuth ClientID is missing");
-//   })
-//     .then(res => {
-//       console.log(res)
-//       setClientID(res.clientID)
-//       // else throw Error("Server error");
-//     })
-//     .catch(err => {
-//       console.log(err.message);
-//     });
-// },[])
-
 //just for testing purpose, will be removed later =========================
 let user = localStorage.getItem("googleEmail")
 
