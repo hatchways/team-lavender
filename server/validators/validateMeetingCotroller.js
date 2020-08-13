612e1b7de8b3a307c8383e16ffa18f19cdbe54eb
atsushiflee <atsushiflee@icloud.com>
2020-08-12 17:14:26 -0700

    Add appointmentId validation

--- server/validators/validateMeetingCotroller.js
+++ server/validators/validateMeetingCotroller.js
@@ -4,9 +4,10 @@ const Meetings = require("../models/Meetings");
 exports.validateCreateNewMeetingReq = async function (req) {
   // init
   let isValid = false;
-  let existUser = false;
+  let existUserAndMeeting = false;
   let message = "";
   const userId = req.body.id;
+  const meetingId = req.body.meetingId;
 
   // Check that id is valid
   const idIsValid = await mongoose.isValidObjectId(userId);
@@ -15,36 +16,47 @@ exports.validateCreateNewMeetingReq = async function (req) {
     isValid = false;
     message = "userId is invalid";
 
-    return { isValid, existUser, message };
+    return { isValid, message, existUserAndMeeting };
   }
 
   // Check that duration is not empty
   if (!req.body.duration) {
     isValid = false;
     message = "duration shouldn't be an empty";
-    return { isValid, existUser, message };
+    return { isValid, message, existUserAndMeeting };
   }
 
   // Check that meetingId is not empty
-  if (!req.body.meetingId) {
+  if (!meetingId) {
     isValid = false;
     message = "meetingId shouldn't be an empty";
-    return { isValid, existUser, message };
+    return { isValid, message, existUserAndMeeting };
   }
 
-  // Check that user has another meeting
-  const user = await Meetings.findById(userId);
+  // Check that appointmentId is not empty
+  if (!req.body.appointmentId) {
+    isValid = false;
+    message = "appointmentId shouldn't be an empty";
+    return { isValid, message, existUserAndMeeting };
+  }
 
-  if (!user) {
-    existUser = false;
+  // Check that user and meetings are existing
+  const user = await Meetings.find({
+    _id: mongoose.Types.ObjectId(userId),
+    "duration.meetingId": meetingId,
+  });
+
+  if (user.length == 0) {
+    existUserAndMeeting = false;
+    isValid = true;
     message = "";
-    return { isValid, existUser, message };
+    return { isValid, message, existUserAndMeeting };
   }
 
   isValid = true;
-  existUser = true;
+  existUserAndMeeting = true;
 
-  return { isValid, existUser, message };
+  return { isValid, message, existUserAndMeeting };
 };
 
 exports.validateLogedInUserId = async function (req) {
