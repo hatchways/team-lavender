b3d0967724a19ffcab20d00eba4f2bdb0450a73b
Mila Mamat <mila.windsor@gmail.com>
2020-08-13 12:42:59 -0400

    seeds for user model testing

---
+++ server/seeds/seedUser.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const db = require("../models/User");
+
+const connectDB = require("../middleware/database");
+connectDB;
+
+const userSeed = [
+  {
+    name: "Mila Test1",
+    email: "mila.test1@gmail.com",
+    avatarUrl: "https://avatars2.githubusercontent.com/u/59339564?v=4",
+
+    timeZone: "America/Toronto",
+    availableHoursFrom: "9:00",
+    availableHoursTo: "17:00",
+    availableDays: [
+      "Mondays",
+      "Tuesdays",
+      "Wednesdays",
+      "Thursdays",
+      "Fridays",
+    ],
+    calendarUrl: "http://localhost:3000/mila-test1"
+  },
+  {
+    name: "Mila Test2",
+    email: "mila.test2@gmail.com",
+    avatarUrl: "https://avatars2.githubusercontent.com/u/59339564?v=4",
+
+    timeZone: "America/Toronto",
+    availableHoursFrom: "9:00",
+    availableHoursTo: "17:00",
+    availableDays: [
+      "Mondays",
+      "Tuesdays",
+      "Wednesdays",
+      "Thursdays",
+      "Fridays",
+    ],
+    calendarUrl: "http://localhost:3000/mila-test2"
+  },
+];
+
+db.insertMany(userSeed)
+  .then(() => {
+    console.log(userSeed.length + " User records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
