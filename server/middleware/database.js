3a2ae1c059721ca6e8f7892af5a8cfb08428e106
Aditya Sharma <sharmadityaa@gmail.com>
2020-08-13 14:34:55 -0400

    Final connection open once using mongoose

--- server/middleware/database.js
+++ server/middleware/database.js
@@ -1,20 +1,17 @@
 const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const database = "calendly";
-const collections = "test";
-
 const uri = process.env.ATLAS_URI;
 
-const client = new MongoClient(uri, {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-client.connect((err) => {
-  if (err) throw err;
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
   console.log("Connected to Atlas MongoDB");
-  const collection = client.db(database).collection(collections);
-  // perform actions on the collection object
-  client.close();
 });
